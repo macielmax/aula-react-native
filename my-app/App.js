1ea2e8a86b4c55fc3c4c05dbc2b2c155102bb2b1
macielmax <maciel.santos@aluno.cps.sp.gov.br>
2026-04-28 14:51:18 +0000

    v1.1

---
+++ my-app/App.js
@@ -0,0 +1,6 @@
+import { View, Text} from 'react-native';
+export default function App() {
+    return (
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}></View>
+    );
+}
